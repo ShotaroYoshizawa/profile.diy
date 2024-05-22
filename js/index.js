3ab27efafd986c4b6968473ed52072951c5ea3b7
shotaro <2b.sytr202@gmail.com>
2024-05-22 22:26:55 +0900

    ロードを追加

--- js/index.js
+++ js/index.js
@@ -49,7 +49,6 @@ function init1() {
         'https://rawcdn.githack.com/ShotaroYoshizawa/profile.diy/a341dc059db5a26075f94cca17a8726ae55d2c03/glb/bed.glb',
         function (glb) {
             model = glb.scene;
-            //model.name = "model_castle";
             model.scale.set(1, 1, 1);
             model.position.set(0, 0, 0);
             scene.add(glb.scene);
@@ -60,8 +59,7 @@ function init1() {
     );
 
     function animate() {
-        requestAnimationFrame(animate);
-        // 回転
+        requestAnimationFrame(animate);// 回転
         model.rotation.y += 0.005;
         renderer.render(scene, camera);
     }
@@ -76,6 +74,8 @@ function init1() {
         requestAnimationFrame(tick);
     }
 }
+
+
 
 function init2() {
     // レンダラーを作成
@@ -418,3 +418,33 @@ function init6() {
         requestAnimationFrame(tick);
     }
 }
+
+
+//lode用
+function load_effect() {
+    var element = document.getElementsByClassName('load-fade');
+    if (!element) return; // 要素がない場合は終了
+  
+    for (var i = 0; i < element.length; i++) {
+      element[i].classList.add('is-show');
+    }
+  }
+  setTimeout(load_effect, 600); // 600ミリ秒経過後に実行
+  
+  //scroll用
+  function scroll_effect() {
+    var element = document.getElementsByClassName('scroll-up');
+    if (!element) return;
+  
+    var scrollY = window.pageYOffset;
+    var windowH = window.innerHeight;
+    var showTiming = 200; // 要素を表示するタイミング
+    for (var i = 0; i < element.length; i++) {
+      var elemClientRect = element[i].getBoundingClientRect();
+      var elemY = scrollY + elemClientRect.top;
+      if (scrollY > elemY - windowH + showTiming) {
+        element[i].classList.add('is-show');
+      }
+    }
+  }
+  window.addEventListener('scroll', scroll_effect); // スクロール時に実行
